36452640ece13a25f4fef1dcfec90fd7b3178fc2
Ben Ellentuck <ellentuckben@gmail.com>
2018-09-16 16:09:17 -0400

    readPlanets method works - read in file, create array of planets

--- uc-berkeley-data-structures/proj0_NBodySimulation/NBody.java
+++ uc-berkeley-data-structures/proj0_NBodySimulation/NBody.java
@@ -1,8 +1,26 @@
 class NBody {
+
+  /**
+  Given a file name, readRadius returns a double corresponding to the radius of the universe in that file.
+  */
   public static double readRadius(String filename) {
     // return the second line in the file.
     In in = new In(filename);
     in.readInt();
     return in.readDouble();
+  }
+
+  /**
+  Given a file name, readPlanets returns an array of Planets corresponding to the planets in the file.
+  */
+  public static Planet[] readPlanets(String filename) {
+    In in = new In(filename);
+    int numPlanets = in.readInt();
+    Planet[] planets = new Planet[numPlanets];
+    in.readDouble(); // discard radius
+    for (int i = 0; i < numPlanets; i++) {
+      planets[i] = new Planet(in.readDouble(), in.readDouble(), in.readDouble(), in.readDouble(), in.readDouble(), in.readString());
+    }
+    return planets;
   }
 }
